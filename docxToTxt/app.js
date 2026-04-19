8c6fbeea2dcf976279a56cc8bd4b46dcaf616015
Lam Minh Man <lamminhmanie@gmail.com>
2026-04-20 00:00:23 +0700

    paragraph, question array

--- docxToTxt/app.js
+++ docxToTxt/app.js
@@ -8,7 +8,7 @@ document.getElementById('upload-docx').addEventListener('change', function(event
         const arrayBuffer = loadEvent.target.result; // Dữ liệu nhị phân của file
         // Sử dụng Mammoth để chuyển sang Plain Text (văn bản thuần)
         // Dùng extractRawText sẽ giúp bạn dễ xử lý thuật toán tách file hơn convertToHtml
-        mammoth.extractRawText({ arrayBuffer: arrayBuffer })
+        mammoth.convertToHtml({ arrayBuffer: arrayBuffer })
             .then(displayResult)
             .catch(handleError);
     };
@@ -24,15 +24,36 @@ function displayResult(result) {
 
     // const arr = text.replace(/<p>/g,"").split("</p>");
     // console.log(arr)
-    const array = text.split(/(READING PASSAGE \d+)/g);
+    const array = text.split(/(<p><strong>READING PASSAGE \d+\t*<\/strong><\/p>)/g);
     console.log(array);
-    const paragraphAndQuestion = []
+    const paragraph = []
+    const question = [];
     for (let index = 2; index < array.length; index += 2) {
-        const element = array[index];
-        paragraphAndQuestion.push(element);
+        const element = array[index].split(/(<\/p><p>\d+.)/g);
+        paragraph.push(array[index - 1])
+        paragraph.push([...element.slice(0,1),"</p>"].join(""))
+        question.push(array[index - 1])
+        question.push(element.slice(1))
     }
-    const paragraph = paragraphAndQuestion[0].split(/(\n\n\n\d+.)/g);
-    console.log(paragraph)
+    
+    
+    question.forEach((element,i) => {
+        if(i % 2 !== 0){
+            element.forEach((value, j) => {
+                if(j % 2 !== 0){
+                    element[j] = [element[j - 1].replace("</p>",""),element[j],"</p>"].join("");
+                }
+            });
+            question[i] = element.filter((_, index) => {
+                return index % 2 !== 0;
+            });
+        }
+    });
+    
+
+    console.log(paragraph);
+    console.log(question);
+    // console.log(paragraphAndQuestion[0].split(/(<\/p><p>\d+.)/g))
     
     
 
