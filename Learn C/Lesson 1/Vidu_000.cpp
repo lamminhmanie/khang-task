8aafcfd82bb83aa85a113a349ab04f1c41605471
Lam Minh Man <lamminhmanie@gmail.com>
2026-05-25 22:51:44 +0700

    lesson 2

--- Learn C/Lesson 1/Vidu_000.cpp
+++ Learn C/Lesson 1/Vidu_000.cpp
@@ -1,1 +1,8 @@
-s
+//Phan 1: Dinh kem thu vien
+#include "stdio.h"
+//Phan 2: Phan khai bao bien, hang
+
+//Phan 3: Cac ham, phuong thuc
+int main(){
+	printf("Xin chao, toi la Man");
+}
---
+++ Learn C/Lesson 2 Kieu du lieu, khai bao bien/Bai so 002.cpp
@@ -0,0 +1,12 @@
+#include "stdio.h"
+int main(){
+	unsigned short tuoi;
+	int so_tien_co;
+	int so_tien = 3000;
+	char y = 'A';
+	long dan_so = 3000000000;
+	double dien_tich;
+	float dien_tich_tam_giac;
+	float x1, x2;
+	float a, b, c;
+}
